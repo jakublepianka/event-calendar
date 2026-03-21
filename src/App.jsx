d73cbc6fcc587137ccc35fb750cd434933d23766
resq-u <jakub12lepianka@wp.pl>
2026-03-21 21:30:19 +0100

    feat: add date picker and Calendar component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { Calendar } from "./Calendar/Calendar";
+
 function App() {
-  return {};
+  const [date, setDate] = useState(new Date());
+  const dateString = date.toISOString().slice(0, 10);
+  
+  function handleDateChange(e) {
+    if (!e) return;
+    setDate(new Date(e.target.value))
+  };
+
+  return (
+    <>
+      <label htmlFor="date">Choose desired month </label>
+      <input type="date" name="monthYear" id="date" onChange={handleDateChange} value={dateString}/>
+      
+      <Calendar date={date} />
+    </>
+  );
 }
 
-export default App
+export default App;
