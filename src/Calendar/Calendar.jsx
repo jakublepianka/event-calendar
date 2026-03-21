13e87d64fa35ef8d1f0d1adf73ad5906feb2f4a6
resq-u <jakub12lepianka@wp.pl>
2026-03-21 21:46:37 +0100

    feat: render calendar days dynamically based on date prop

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,6 +1,14 @@
-export function Calendar() {
+export function Calendar({ date }) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const monthLength = new Date(year, month + 1, 0).getDate();
+
   return (
     <>
+    {Array.from({ length: monthLength }).map((day, index) => { 
+      const dayNum = index + 1;
+      return <div key={dayNum}>{dayNum}</div>;
+    })}
     </>
   )
 }
