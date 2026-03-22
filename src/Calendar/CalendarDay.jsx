74821fd1e1c528f5e61deac86235642fd10cec0d
resq-u <jakub12lepianka@wp.pl>
2026-03-22 22:46:47 +0100

    feat(calendar-day): add styling and improve layout

--- src/Calendar/CalendarDay.jsx
+++ src/Calendar/CalendarDay.jsx
@@ -1,36 +1,39 @@
 import { Link } from "react-router";
+import styles from "./CalendarDay.module.css";
 
 export function CalendarDay({ dayNum, events }) {
   return (
-    <>
-      <div>{dayNum}</div>
-      {events.length > 0 && (
-        <ul>
-          {events.map((event, eventIndex) => {
-            const homeTeamAbbrev = event.homeTeam
-              ? event.homeTeam.abbreviation
-              : "N/A";
-            const awayTeamAbbrev = event.awayTeam
-              ? event.awayTeam.abbreviation
-              : "N/A";
+    <div className={styles.calendarDay}>
+      <div className={styles.dayNum}>{dayNum}</div>
+      <div>
+        {events.length > 0 && (
+          <ul className={styles.eventList}>
+            {events.map((event, eventIndex) => {
+              const homeTeamAbbrev = event.homeTeam
+                ? event.homeTeam.abbreviation
+                : "N/A";
+              const awayTeamAbbrev = event.awayTeam
+                ? event.awayTeam.abbreviation
+                : "N/A";
 
-            return (
-              <li key={eventIndex}>
-                <p>{event.originCompetitionName}</p>
-                <p>
-                  {homeTeamAbbrev} vs {awayTeamAbbrev}
-                </p>
-                <Link
-                  to={`/details/${event.dateVenue}-${homeTeamAbbrev}-${awayTeamAbbrev}-${event.timeVenueUTC}`}
-                  state={{ event }}
-                >
-                  See details
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </>
+              return (
+                <li className={styles.eventItem} key={eventIndex}>
+                  <Link
+                    aria-label="See event details"
+                    className={styles.detailsLink}
+                    to={`/details/${event.dateVenue.slice(2)}-${event.timeVenueUTC.slice(0, 5)}-${eventIndex}`}
+                    state={{ event }}
+                  >
+                    <p>
+                      {homeTeamAbbrev} vs {awayTeamAbbrev}
+                    </p>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
