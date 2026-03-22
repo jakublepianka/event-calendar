f7f8eacf698ca9cf1d0031908b7aa55c2f2fd590
resq-u <jakub12lepianka@wp.pl>
2026-03-22 21:06:08 +0100

    feat(calendar): refactor markup and add CSS modules

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -2,27 +2,47 @@ import { useState } from "react";
 import { CalendarGrid } from "./CalendarGrid";
 import { Link } from "react-router";
 import { useOutletContext } from "react-router";
+import styles from "./Calendar.module.css";
 
 export function Calendar() {
-  const { events: allEvents} = useOutletContext();
+  const { events: allEvents } = useOutletContext();
   const [selectedDate, setSelectedDate] = useState(new Date());
   const selectedDateString = selectedDate.toISOString().slice(0, 10);
-  const monthlyEvents = allEvents.filter(event => { 
+  const monthlyEvents = allEvents.filter((event) => {
     const eventMonthString = event.dateVenue.slice(0, -3);
     const selectedMonthString = selectedDateString.slice(0, -3);
-    return eventMonthString === selectedMonthString; 
-  })
+    return eventMonthString === selectedMonthString;
+  });
   function onDateChange(event) {
     if (!event) return;
     const newDate = new Date(event.target.value);
     setSelectedDate(newDate);
-  };
+  }
   return (
     <>
-      <label htmlFor="date">Choose desired month </label>
-      <input type="date" name="monthYear" id="date" onChange={onDateChange} value={selectedDateString}/>
-      <Link to="add-event">Add new event</Link>
-      <CalendarGrid date={selectedDate} events={monthlyEvents} />
+      <div className={styles.calendar}>
+        <header className={styles.calendarHeader}>
+          <nav className={styles.calendarNav}>
+            <Link to="add-event" className={styles.addEventLink}>
+              Create event
+            </Link>
+          </nav>
+
+          <div className={styles.calendarControls}>
+            <label htmlFor="date">Select month</label>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              onChange={onDateChange}
+              value={selectedDateString}
+            />
+          </div>
+        </header>
+        <main className={styles.calendarContent}>
+          <CalendarGrid date={selectedDate} events={monthlyEvents} />
+        </main>
+      </div>
     </>
   );
 }
